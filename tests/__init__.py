c8863a6e18f860f3fbcbdc3ff47b2f3197649ff3
Eric Heller <eheller@gmail.com>
2016-05-15 17:54:14 -0500

    Add __init__ file to /tests/ directory.

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Tests for fastavro"""
+
+from __future__ import absolute_import
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, 'fastavro')
+
+import fastavro  # noqa
