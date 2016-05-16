1325cc316e17bcaef6b2f3ed96c86d941167f6bb
Eric Heller <eheller@gmail.com>
2016-05-15 20:22:11 -0500

    Update __init__. Fix up doc string and code examples. Add aliases 'fastavro.Reader' and 'fastavro.write'. Bump version to 0.9.10

--- fastavro/__init__.py
+++ fastavro/__init__.py
@@ -1,59 +1,67 @@
 # -*- coding: utf-8 -*-
-'''Fast Avro file iteration.
+"""Fast Avro - faster reading and writing Avro files.
 
-Most of the code here is ripped off the Python avro package. It's missing a lot
-of features in order to get speed.
+To read an Avro file, a simple API is exposed: `fastavro.Reader`
 
-The only onterface function is iter_avro, example usage::
+Example usage:
+>>> import fastavro
+>>>
+>>> with open('some_file.avro', 'rb') as input:
+>>>     # Create a `Reader` object
+>>>     reader = fastavro.Reader(input)
+>>>
+>>>     # Obtain the writer's schema if required
+>>>     schema = reader.schema
+>>>
+>>>     # Iteratively read the records:
+>>>     for record in reader:
+>>>         process_record(record)
+>>>
+>>>     # Read the records in one shot:
+>>>     records = list(reader)
 
-    # Reading
-    import fastavro as avro
+To write an Avro file, a simple API is exposed: `fastavro.write`
 
-    with open('some-file.avro', 'rb') as fo:
-        reader = fastavro.reader(fo)
-        schema = reader.schema
-
-        for record in reader:
-            process_record(record)
-
-
-    # Writing
-    from fastavro import writer
-
-    schema = {
-        'doc': 'A weather reading.',
-        'name': 'Weather',
-        'namespace': 'test',
-        'type': 'record',
-        'fields': [
-            {'name': 'station', 'type': 'string'},
-            {'name': 'time', 'type': 'long'},
-            {'name': 'temp', 'type': 'int'},
-        ],
-    }
-
-    records = [
-        {u'station': u'011990-99999', u'temp': 0, u'time': 1433269388},
-        {u'station': u'011990-99999', u'temp': 22, u'time': 1433270389},
-        {u'station': u'011990-99999', u'temp': -11, u'time': 1433273379},
-        {u'station': u'012650-99999', u'temp': 111, u'time': 1433275478},
-    ]
-
-    with open('weather.avro', 'wb') as out:
-        writer(out, schema, records)
-'''
+Example usage:
+>>> import fastavro
+>>>
+>>> # Define an Avro Schema as a dict:
+>>> schema = {
+>>>     'doc': 'A weather reading.',
+>>>     'name': 'Weather',
+>>>     'namespace': 'test',
+>>>     'type': 'record',
+>>>     'fields': [
+>>>         {'name': 'station', 'type': 'string'},
+>>>         {'name': 'time', 'type': 'long'},
+>>>         {'name': 'temp', 'type': 'int'},
+>>>     ],
+>>> }
+>>>
+>>> # Create some records:
+>>> records = [
+>>>     {u'station': u'011990-99999', u'temp': 0, u'time': 1433269388},
+>>>     {u'station': u'011990-99999', u'temp': 22, u'time': 1433270389},
+>>>     {u'station': u'011990-99999', u'temp': -11, u'time': 1433273379},
+>>>     {u'station': u'012650-99999', u'temp': 111, u'time': 1433275478},
+>>> ]
+>>>
+>>> # Open a file, and write the records:
+>>> with open('weather.avro', 'wb') as out:
+>>>     fastavro.write(out, schema, records)
+"""
 
 from __future__ import absolute_import
 
 
-__version__ = '0.9.9'
+__version__ = '0.9.10'
 
 
 try:
     from . import _reader
     from . import _writer
     from . import _schema
-except ImportError as e:
+except ImportError:
     from . import reader as _reader
     from . import writer as _writer
     from . import schema as _schema
@@ -74,12 +82,12 @@ def _acquaint_schema(schema):
 acquaint_schema = _schema.acquaint_schema = _acquaint_schema
 
 # Reader API
-reader = iter_avro = _reader.Reader
+Reader = reader = iter_avro = _reader.Reader
 schemaless_reader = _reader.schemaless_reader
 load = _reader.read_data
 
 # Writer API
-writer = _writer.writer
+write = writer = _writer.writer
 schemaless_writer = _writer.schemaless_writer
 dump = _writer.write_data
 
