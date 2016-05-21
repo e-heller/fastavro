94ae17b75eead9ce76695c2f2b482f02077ebf38
Eric Heller <eheller@gmail.com>
2016-05-21 17:50:26 -0500

    Update setup.py to build new pure-Cython extension modules. Also, give the file a bit of make-over.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
 try:
     from setuptools import setup
 except ImportError:
@@ -11,72 +13,77 @@ from distutils.core import Extension
 from distutils.errors import (
     CCompilerError, DistutilsExecError, DistutilsPlatformError
 )
+from distutils import log
 from os.path import join
-import distutils.log as log
-import re
-import sys
 
 
-def extension(base):
-    cmodule = '_{0}'.format(base)
-    cfile = join('fastavro', '{0}.c'.format(cmodule))
-
-    return Extension('fastavro.{0}'.format(cmodule), [cfile])
+pkg = 'fastavro'
 
 
 def version():
-    pyfile = 'fastavro/__init__.py'
-    with open(pyfile) as fp:
-        data = fp.read()
-
-    match = re.search("__version__ = '([^']+)'", data)
-    assert match, 'cannot find version in {}'.format(pyfile)
-    return match.group(1)
-
-
-ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError,
-              IOError)
-
-
-class maybe_build_ext(build_ext):
-    """This class allows C extension building to fail."""
-
-    def run(self):
-        try:
-            build_ext.run(self)
-        except DistutilsPlatformError:
-            log.info('cannot bulid C extension, will continue without.')
-
-    def build_extension(self, ext):
-        try:
-            build_ext.build_extension(self, ext)
-        except ext_errors:
-            log.info('cannot bulid C extension, will continue without.')
+    import re
+    init_file = open(join(pkg, '__init__.py'), 'r').read()
+    match = re.search(r'__version_info__\s*=\s*\((.+?)\)', init_file)
+    assert match, 'Failed to find fastavro __version__'
+    return '.'.join(match.group(1).replace(' ', '').split(','))
 
 
 install_requires = []
 if sys.version_info[:2] < (2, 7):
-    # `argparse` is not in the standard library for Python < 2.7
+    # `argparse` is not in the Python 2.6 standard library
     install_requires.append('argparse')
 
 
-tests_require = ['nose', 'flake8']
+tests_require = ['nose', 'flake8', 'cython']
 if sys.version_info[0] < 3:
-    # Some tests require 'unittest2' on Python 2.x
+    # Some tests require `unittest2` for Python 2.x
     tests_require.append('unittest2')
 
 
-# Don't compile extension under pypy
-# See https://bitbucket.org/pypy/pypy/issue/1770
-ext_modules = [
-    extension('reader'),
-    extension('compat'),
-    extension('writer'),
-    extension('schema'),
-]
+class maybe_build_ext(build_ext):
+    """Allow building Cython extension modules to fail gracefully"""
+
+    build_errors = (
+        CCompilerError, DistutilsExecError, DistutilsPlatformError, IOError
+    )
+
+    def run(self):
+        try:
+            build_ext.run(self)
+        except self.build_errors:
+            log.warn('Failed to build Cython extension modules')
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except self.build_errors:
+            log.warn('Failed to build Cython extension modules')
+
+
+def _extensions(*module_names):
+    return [
+        Extension(module, sources=['%s.c' % module])
+        for module in (
+            join(pkg, module_name) for module_name in module_names
+        )
+    ]
+
+
+ext_modules = _extensions(
+    'c_reader',
+    'c_writer',
+    'c_schema',
+    'c_utils',
+    'c_buffer',
+)
+
 if hasattr(sys, 'pypy_version_info'):
+    # Don't compile extension under pypy
+    # See https://bitbucket.org/pypy/pypy/issue/1770
     ext_modules = []
 
+
+# ---- Setup -----------------------------------------------------------------#
 
 setup(
     name='fastavro',
@@ -87,15 +94,30 @@ setup(
     author_email='eheller@gmail.com',
     license='MIT',
     url='https://github.com/e-heller/fastavro',
-    packages=['fastavro'],
+
+    packages=[pkg],
+    zip_safe=False,
     ext_modules=ext_modules,
     cmdclass={'build_ext': maybe_build_ext},
-    zip_safe=False,
+
+    # Include files from MANIFEST.in
+    include_package_data=True,
+
+    # Command-line script
     entry_points={
         'console_scripts': [
             'fastavro = fastavro.__main__:main',
         ]
     },
+
+    # Requirements
+    install_requires=install_requires,
+    extras_require={
+        'snappy': ['python-snappy'],
+        'simplejson': ['simplejson'],
+    },
+    tests_require=tests_require,
+
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -106,10 +128,4 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires=install_requires,
-    extras_require={
-        'snappy': ['python-snappy'],
-        'simplejson': ['simplejson'],
-    },
-    tests_require=tests_require,
 )
