3318b09ba5b36abbb65d22269b80c2e0cd19c68b
Eric Heller <eheller@gmail.com>
2016-05-22 17:55:47 -0500

    Refactor `test_byte_buffer` to work correctly with Python 2 and 3. For some reason, I don't know why, nosetests fails to find any "test" functions when importing a TestCase from the .pyx file. So, instead, we will just dynamically create the TestCase class.  This is really ugly, I know! For now it works ok.

--- tests/_test_byte_buffer.pyx
+++ tests/_test_byte_buffer.pyx
@@ -3,9 +3,11 @@
 """Tests for fastavro.c_buffer (ByteBuffer class)"""
 
 
-# NOTE: The ByteBuffer unit tests must be written in a Cython .pyx file,
-# and then compiled. The TestCases defined in this module are then imported
-# in `test_byte_buffer.py` to actually run the tests.
+# NOTE: The ByteBuffer unit tests must be written in a Cython .pyx file, and
+# then compiled. The test functions defined in this module are intended to be
+# methods in a `unittest.TestCase` instance. The test file `test_byte_buffer`
+# imports each 'test_*' function defined here and dynamically adds them to a
+# `unittest.TestCase` for actual testing.
 
 # TODO: Finish writing tests!
 
@@ -14,184 +16,182 @@ from __future__ import absolute_import
 
 import sys
 
+# Import unittest module (requires `unittest2` for Python 2.x)
+#if sys.version_info[0] == 2:
+#    try:
+#        import unittest2 as unittest
+#    except ImportError:
+#        raise ImportError("The 'unittest2' module is required for Python 2.x")
+#else:
+#    import unittest
+
 from fastavro.c_buffer cimport (
     Stream, ByteBuffer, cSEEK_SET, cSEEK_CUR, cSEEK_END, SSize_t, uchar,
 )
 
-# Import unittest module (requires `unittest2` for Python 2.x)
-if sys.version_info[0] == 2:
-    try:
-        import unittest2 as unittest
-    except ImportError:
-        raise ImportError("The 'unittest2' module is required for Python 2.x")
-else:
-    import unittest
 
+def test_create_and_close_buffer(self):
+    cdef ByteBuffer buf
+    buf = ByteBuffer(20)
+    self.assertIsInstance(buf, Stream)
+    self.assertEqual(buf.size(), 20)
+    self.assertEqual(len(buf), 0)
+    self.assertEqual(buf.tell(), 0)
+    self.assertEqual(buf.getvalue(), b'')
 
-class TestByteBuffer(unittest.TestCase):
+    buf.close()
+    self.assertTrue(buf.closed)
 
-    def test_create_and_close_buffer(self):
-        cdef ByteBuffer buf
-        buf = ByteBuffer(20)
-        self.assertIsInstance(buf, Stream)
-        self.assertEqual(buf.size(), 20)
-        self.assertEqual(len(buf), 0)
-        self.assertEqual(buf.tell(), 0)
-        self.assertEqual(buf.getvalue(), b'')
+    # close() may be called multiple times
+    buf.close()
+    self.assertTrue(buf.closed)
 
-        buf.close()
-        self.assertTrue(buf.closed)
+    # These should all be zero
+    self.assertEqual(buf.pos, 0)
+    self.assertEqual(buf.buf_len, 0)
+    self.assertEqual(buf.data_len, 0)
 
-        # close() may be called multiple times
-        buf.close()
-        self.assertTrue(buf.closed)
+    # The corresponding methods should return zero when closed
+    self.assertEqual(buf.tell(), 0)
+    self.assertEqual(buf.size(), 0)
+    self.assertEqual(len(buf), 0)
 
-        # These should all be zero
-        self.assertEqual(buf.pos, 0)
-        self.assertEqual(buf.buf_len, 0)
-        self.assertEqual(buf.data_len, 0)
+    # Other methods should raise an exception
+    with self.assertRaises(ValueError):
+        buf.getvalue()
+    with self.assertRaises(ValueError):
+        buf.write(b'foo')
+    with self.assertRaises(ValueError):
+        buf.read(1)
 
-        # The corresponding methods should return zero when closed
-        self.assertEqual(buf.tell(), 0)
-        self.assertEqual(buf.size(), 0)
-        self.assertEqual(len(buf), 0)
+def test_create_buffer_no_size_arg(self):
+    cdef ByteBuffer buf
+    buf = ByteBuffer()
+    # ByteBuffer with no size argument will start with a default size
+    self.assertTrue(buf.size())
+    self.assertEqual(len(buf), 0)
+    self.assertEqual(buf.tell(), 0)
+    self.assertEqual(buf.getvalue(), b'')
 
-        # Other methods should raise an exception
-        with self.assertRaises(ValueError):
-            buf.getvalue()
-        with self.assertRaises(ValueError):
-            buf.write(b'foo')
-        with self.assertRaises(ValueError):
-            buf.read(1)
+    buf.close()
 
-    def test_create_buffer_no_size_arg(self):
-        cdef ByteBuffer buf
-        buf = ByteBuffer()
-        # ByteBuffer with no size argument will start with a default size
-        self.assertTrue(buf.size())
-        self.assertEqual(len(buf), 0)
-        self.assertEqual(buf.tell(), 0)
-        self.assertEqual(buf.getvalue(), b'')
+def test_buffer_write(self):
+    cdef ByteBuffer buf
+    buf = ByteBuffer(16)
+    original_size = buf.size()
 
-        buf.close()
+    # Writing null bytes -
+    buf.write(b'')
+    self.assertEqual(buf.tell(), 0)
+    self.assertEqual(buf.getvalue(), b'')
 
-    def test_buffer_write(self):
-        cdef ByteBuffer buf
-        buf = ByteBuffer(16)
-        original_size = buf.size()
+    # Write 10 bytes
+    value1 = b'0123456789'
+    buf.write(value1)
+    tell1 = buf.tell()
+    self.assertEqual(tell1, len(value1))
+    self.assertEqual(len(buf), tell1)
+    self.assertEqual(buf.getvalue(), value1)
+    # Writing 10 bytes should not resize the buffer
+    self.assertEqual(buf.size(), original_size)
 
-        # Writing null bytes -
-        buf.write(b'')
-        self.assertEqual(buf.tell(), 0)
-        self.assertEqual(buf.getvalue(), b'')
+    # Edge case - write 6 bytes
+    value2 = b'012345'
+    buf.write(value2)
+    tell2 = buf.tell()
+    self.assertEqual(tell2, tell1 + len(value2))
+    self.assertEqual(len(buf), tell2)
+    self.assertEqual(buf.getvalue(), value1 + value2)
+    # Writing 6 bytes WILL cause the buffer to resize -- Even though we
+    # wrote a total of 16 bytes and the buffer size is also exactly 16
+    # bytes -- The buffer will resize whenever buf.data_len >= buf.buf_len
+    self.assertNotEqual(buf.size(), original_size)
+    # The buffer will be resized to the next power of 2 > 16 * 2 == 32
+    self.assertEqual(buf.size(), 32)
 
-        # Write 10 bytes
-        value1 = b'0123456789'
-        buf.write(value1)
-        tell1 = buf.tell()
-        self.assertEqual(tell1, len(value1))
-        self.assertEqual(len(buf), tell1)
-        self.assertEqual(buf.getvalue(), value1)
-        # Writing 10 bytes should not resize the buffer
-        self.assertEqual(buf.size(), original_size)
+    # Write 20 bytes
+    value3 = b'x' * 20
+    buf.write(value3)
+    tell3 = buf.tell()
+    self.assertEqual(tell3, tell2 + len(value3))
+    self.assertEqual(len(buf), tell3)
+    self.assertEqual(buf.getvalue(), value1 + value2 + value3)
+    # Writing 20 bytes will cause the buffer to be resized: 16 + 20 = 36
+    # The buffer will be resized to next power of 2 > 36 * 2 == 128
+    self.assertEqual(buf.size(), 128)
 
-        # Edge case - write 6 bytes
-        value2 = b'012345'
-        buf.write(value2)
-        tell2 = buf.tell()
-        self.assertEqual(tell2, tell1 + len(value2))
-        self.assertEqual(len(buf), tell2)
-        self.assertEqual(buf.getvalue(), value1 + value2)
-        # Writing 6 bytes WILL cause the buffer to resize -- Even though we
-        # wrote a total of 16 bytes and the buffer size is also exactly 16
-        # bytes -- The buffer will resize whenever buf.data_len >= buf.buf_len
-        self.assertNotEqual(buf.size(), original_size)
-        # The buffer will be resized to the next power of 2 > 16 * 2 == 32
-        self.assertEqual(buf.size(), 32)
+    # Seek to zero, overwrite old data with 100 bytes
+    value4 = b'z' * 100
+    buf.seek(0)
+    buf.write(value4)
+    tell4 = buf.tell()
+    self.assertEqual(tell4, len(value4))
+    self.assertEqual(len(buf), tell4)
+    self.assertEqual(buf.getvalue(), value4)
+    self.assertEqual(buf.size(), 128)
 
-        # Write 20 bytes
-        value3 = b'x' * 20
-        buf.write(value3)
-        tell3 = buf.tell()
-        self.assertEqual(tell3, tell2 + len(value3))
-        self.assertEqual(len(buf), tell3)
-        self.assertEqual(buf.getvalue(), value1 + value2 + value3)
-        # Writing 20 bytes will cause the buffer to be resized: 16 + 20 = 36
-        # The buffer will be resized to next power of 2 > 36 * 2 == 128
-        self.assertEqual(buf.size(), 128)
+    buf.close()
 
-        # Seek to zero, overwrite old data with 100 bytes
-        value4 = b'z' * 100
-        buf.seek(0)
-        buf.write(value4)
-        tell4 = buf.tell()
-        self.assertEqual(tell4, len(value4))
-        self.assertEqual(len(buf), tell4)
-        self.assertEqual(buf.getvalue(), value4)
-        self.assertEqual(buf.size(), 128)
+def test_buffer_write_chars(self):
+    cdef ByteBuffer buf
+    buf = ByteBuffer(16)
+    original_size = buf.size()
 
-        buf.close()
+    cdef bytes value1 = b'0123456789'
+    cdef uchar* char_value1 = value1
+    buf.write_chars(char_value1, len(value1))
+    tell1 = buf.tell()
+    self.assertEqual(tell1, len(value1))
+    self.assertEqual(len(buf), len(value1))
+    self.assertEqual(buf.getvalue(), value1)
+    # Writing 10 bytes should not resize the buffer
+    self.assertEqual(buf.size(), original_size)
 
-    def test_buffer_write_chars(self):
-        cdef ByteBuffer buf
-        buf = ByteBuffer(16)
-        original_size = buf.size()
+    cdef bytes value2 = value1 * 3
+    cdef uchar* char_value2 = value2
+    buf.write_chars(char_value2, len(value2))
+    self.assertEqual(buf.tell(), tell1 + len(value2))
+    self.assertEqual(len(buf), len(value1 + value2))
+    self.assertEqual(buf.getvalue(), value1 + value2)
+    # Writing 30 bytes should resize the buffer, 10 + 30 = 40 > 16
+    # The buffer will be resized to nearest power of 2 for 40 * 2 => 128
+    self.assertEqual(buf.size(), 128)
 
-        cdef bytes value1 = b'0123456789'
-        cdef uchar* char_value1 = value1
-        buf.write_chars(char_value1, len(value1))
-        tell1 = buf.tell()
-        self.assertEqual(tell1, len(value1))
-        self.assertEqual(len(buf), len(value1))
-        self.assertEqual(buf.getvalue(), value1)
-        # Writing 10 bytes should not resize the buffer
-        self.assertEqual(buf.size(), original_size)
+    buf.close()
 
-        cdef bytes value2 = value1 * 3
-        cdef uchar* char_value2 = value2
-        buf.write_chars(char_value2, len(value2))
-        self.assertEqual(buf.tell(), tell1 + len(value2))
-        self.assertEqual(len(buf), len(value1 + value2))
-        self.assertEqual(buf.getvalue(), value1 + value2)
-        # Writing 30 bytes should resize the buffer, 10 + 30 = 40 > 16
-        # The buffer will be resized to nearest power of 2 for 40 * 2 => 128
-        self.assertEqual(buf.size(), 128)
+def test_buffer_seek(self):
+    cdef ByteBuffer buf
+    buf = ByteBuffer(2)
 
-        buf.close()
+    cdef bytes value = b'0123456789' * 2
+    buf.write(value)
+    self.assertEqual(buf.tell(), len(value))
+    self.assertEqual(buf.getvalue(), value)
 
-    def test_buffer_seek(self):
-        cdef ByteBuffer buf
-        buf = ByteBuffer(2)
+    buf.seek(0)
+    self.assertEqual(buf.tell(), 0)
+    # `buf.getvalue` depends on the seek position
+    self.assertEqual(buf.getvalue(), b'')
 
-        cdef bytes value = b'0123456789' * 2
-        buf.write(value)
-        self.assertEqual(buf.tell(), len(value))
-        self.assertEqual(buf.getvalue(), value)
+    buf.seek(10)
+    self.assertEqual(buf.tell(), 10)
+    # `buf.getvalue` depends on the seek position
+    self.assertEqual(buf.getvalue(), value[10:])
 
-        buf.seek(0)
-        self.assertEqual(buf.tell(), 0)
-        # `buf.getvalue` depends on the seek position
-        self.assertEqual(buf.getvalue(), b'')
+    buf.seek(20)
+    self.assertEqual(buf.tell(), 20)
+    self.assertEqual(buf.getvalue(), value)
 
-        buf.seek(10)
-        self.assertEqual(buf.tell(), 10)
-        # `buf.getvalue` depends on the seek position
-        self.assertEqual(buf.getvalue(), value[10:])
+    # Should be able to seek to P for 0 <= P <= buf.buf_len:
+    for P in range(0, buf.buf_len + 1):
+        buf.seek(P)
 
-        buf.seek(20)
-        self.assertEqual(buf.tell(), 20)
-        self.assertEqual(buf.getvalue(), value)
+    # Cannot seek < 0
+    with self.assertRaises(ValueError):
+        buf.seek(-1)
 
-        # Should be able to seek to P for 0 <= P <= buf.buf_len:
-        for P in range(0, buf.buf_len + 1):
-            buf.seek(P)
+    # Cannot seek > buf.buf_len
+    with self.assertRaises(ValueError):
+        buf.seek(buf.buf_len + 1)
 
-        # Cannot seek < 0
-        with self.assertRaises(ValueError):
-            buf.seek(-1)
-
-        # Cannot seek > buf.buf_len
-        with self.assertRaises(ValueError):
-            buf.seek(buf.buf_len + 1)
-
-        buf.close()
+    buf.close()
--- tests/test_byte_buffer.py
+++ tests/test_byte_buffer.py
@@ -2,24 +2,15 @@
 """Tests for fastavro.c_buffer (ByteBuffer class)"""
 
 
-# NOTE: The ByteBuffer unit tests must be written in a Cython .pyx file,
-# and then compiled. In this module, we import the unittest.TestCase classes
-# defined in `_test_byte_buffer`, and implement the `load_tests` function so
-# nosetests can find and run the tests. Yes, it's a little hairy.
+# NOTE: The ByteBuffer unit tests must be written in a Cython .pyx file, and
+# then compiled. In this module, we create a `unittest.TestCase` class and
+# dynamically add each 'test_*' function defined in `_test_byte_buffer`.
+# This is very hairy, I know.
 
 
 from __future__ import absolute_import
 
 import sys
-
-try:
-    from tests._test_byte_buffer import TestByteBuffer
-except ImportError:
-    TestByteBuffer = None
-    sys.stderr.write(
-        'Failed to import compiled test `_test_byte_buffer.pyx`: ' +
-        '`test_byte_buffer` tests will not run.\n'
-    )
 
 # Import unittest module (requires `unittest2` for Python 2.x)
 if sys.version_info[0] == 2:
@@ -30,29 +21,32 @@ if sys.version_info[0] == 2:
 else:
     import unittest
 
-
-_test_cases = (TestByteBuffer,)
-
-
-def load_tests(*args):
-    # If we were using `unittest.main`, then this function would receive
-    # 3 arguments `(loader, tests, pattern)`
-    #   cf: https://docs.python.org/3/library/unittest.html#load-tests-protocol
-    # However, nosetests doesn't seem to implement this properly and passes
-    # no arguments to `load_tests`. In this case we just need to create our
-    # own `TestLoader` instance, then build and return our TestSuite.
-    if args:
-        loader, _, _ = args
-    else:
-        loader = unittest.TestLoader()
-    suite = unittest.TestSuite()
-    for case in filter(None, _test_cases):
-        tests = loader.loadTestsFromTestCase(case)
-        suite.addTests(tests)
-    return suite
+try:
+    from tests import _test_byte_buffer
+except ImportError:
+    _test_byte_buffer = None
+    sys.stderr.write(
+        'Failed to import compiled test `_test_byte_buffer.pyx`: ' +
+        '`test_byte_buffer` tests will not run.\n'
+    )
 
 
-if __name__ == '__main__':
-    import nose
-    config = nose.config.Config(logStream=sys.stdout, stream=sys.stdout)
-    nose.runmodule(config=config)
+class TestByteBuffer(unittest.TestCase):
+    pass
+
+
+def setup():
+    if not _test_byte_buffer:
+        return
+    module = _test_byte_buffer
+    test_funcs = [f for f in dir(module) if f.startswith('test_')]
+    for f in test_funcs:
+        func = getattr(module, f)
+
+        def wrap(self):
+            func(self)
+
+        name = wrap.__name__ = func.__name__
+        setattr(TestByteBuffer, name, wrap)
+
+setup()
